a94f8a0d59c07e315ea224d69a3927e738af7c09
Peter Lemiszki <plemiszki@gmail.com>
2019-10-10 17:49:55 -0400

    add templates, layout, favicon

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'homepage'
+    return render_template('home.html')
 
 @app.route('/favorites')
 def favorites():
-    return 'favorites'
+    return render_template('favorites.html')
 
 @app.route('/play')
 def play():
-    return 'play'
+    return render_template('play.html')
