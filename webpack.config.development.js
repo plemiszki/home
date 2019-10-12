0ebae7989db8919dbc9f912044166b6469be81a7
Peter Lemiszki <plemiszki@gmail.com>
2019-10-12 13:57:33 -0400

    set up webpack

---
+++ webpack.config.development.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: 'development',
+  context: __dirname,
+  entry: './frontend/entry.jsx',
+  output: {
+    path: __dirname + '/app/static',
+    filename: 'bundle.js',
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
