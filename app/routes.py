965733ef30f833ba523fb4a1bb57f067476747b1
Peter Lemiszki <plemiszki@gmail.com>
2019-10-19 15:18:59 -0400

    kill child process actually playing song

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,8 @@ def play(album_id):
     process_ids = list(redis_client.smembers('processes'))
     for process_id in process_ids:
         process_id = process_id.decode("utf-8")
-        Popen(['kill', '-9', process_id])
+        child_process_id = os.popen(f"ps --ppid {process_id} -o pid=").read().split("\n")[0].strip()
+        os.system(f"kill -9 {child_process_id}")
     redis_client.delete('processes')
     # play first song
     album = Album.query.get(album_id)
