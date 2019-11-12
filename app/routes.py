06f74a64e770dd81ebfbd806e1b780c0e8b013f4
Peter Lemiszki <plemiszki@gmail.com>
2019-11-11 20:31:06 -0500

    stop song on homepage

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ import os
 import json
 from app import app, db
 from flask import render_template, jsonify, request
+from app.utils import stop_everything
 from app.models import Album
 from to_camel_case import to_camel_case
 from subprocess import Popen
@@ -11,19 +12,13 @@ redis_client = FlaskRedis(app)
 @app.route('/')
 @app.route('/home')
 def home():
+    stop_everything()
     albums = Album.query.order_by('artist_name', 'name').all()
     return render_template('home.html', albums=albums)
 
 @app.route('/play/<album_id>')
 def play(album_id):
-    # stop any playing songs
-    process_ids = list(redis_client.smembers('processes'))
-    for process_id in process_ids:
-        process_id = process_id.decode("utf-8")
-        child_process_id = os.popen(f"ps --ppid {process_id} -o pid=").read().split("\n")[0].strip()
-        os.system(f"kill -9 {child_process_id}")
-    redis_client.delete('processes')
-    # play first song
+    stop_everything()
     album = Album.query.get(album_id)
     music_directory = os.getenv('MUSIC_DIRECTORY')
     filenames = os.listdir(f"{music_directory}/{album.artist_name}/{album.name}")
