7bc3894fad4099c8b1eca79de9f8d3bc1e8a0263
Peter Lemiszki <plemiszki@gmail.com>
2020-03-15 13:01:49 -0400

    groundwork for reading temperature

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import json
 import random
 import requests
@@ -15,9 +16,17 @@ redis_client = FlaskRedis(app)
 from datetime import datetime, timezone, timedelta
 from dateutil import tz
 
+if app.config['ENV'] == 'production':
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
+
 @app.route('/')
 def home():
-    return render_template('public/home.html')
+    temp_f, temp_c = read_temp() if app.config['ENV'] == 'production' else ['TEMP_F', 'TEMP_C']
+    return render_template('public/home.html', temp_f=temp_f, temp_c=temp_c)
 
 @app.route('/music/modern')
 def music_modern():
@@ -213,3 +222,21 @@ def process_mta_response(response, feed, train, output):
                                     'leave_in': leave_in
                                 })
     return output
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
