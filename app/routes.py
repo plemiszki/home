31664d15cdf44cf5b211ffc810b522a5782798d8
Peter Lemiszki <plemiszki@gmail.com>
2026-03-15 21:27:22 -0400

    temp sensor env var

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ redis_client = FlaskRedis(app)
 from datetime import datetime, timezone, timedelta
 from dateutil import tz
 
-if os.environ.get('FLASK_ENV') == 'production':
+if os.environ.get('FLASK_ENV') == 'production' and os.environ.get('TEMP_SENSOR_ENABLED') == 'true':
     os.system('modprobe w1-gpio')
     os.system('modprobe w1-therm')
     base_dir = '/sys/bus/w1/devices/'
